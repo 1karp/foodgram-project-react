73e5339306ab71fafa31b2c38728b75067ac3d97
Artem Karpov <ikarp97@gmail.com>
2023-03-22 11:58:18 +0400

    added models.py

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,3 +1,88 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+from users.models import User
+
+
+class Ingredient(models.Model):
+
+    name = models.CharField('Название ингредиента', max_length=200)
+    measurement_unit = models.CharField('Единица измерения', max_length=200)
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Тег')
+    color = models.CharField(max_length=7, verbose_name='Цвет')
+    slug = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = 'Тег'
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+
+    author = models.ForeignKey(
+        User,
+        related_name='recipes',
+        verbose_name='Автор'
+    )
+
+    name = models.CharField('Название', max_length=200)
+
+    image = models.ImageField(
+        verbose_name='Изображение',
+        upload_to='recipes/'
+    )
+
+    text = models.TextField('Описание', max_length=1000)
+
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='RecipeIngredient',
+        related_name='recipes',
+        verbose_name='Ингредиенты'
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        related_name='recipes',
+        verbose_name='Тег'
+    )
+
+    cooking_time = models.IntegerField(
+        verbose_name='Время приготовления',
+        validators=[MinValueValidator(1)],
+        )
+
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'Рецепт'
+
+    def __str__(self):
+        return self.name
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE
+    )
+    amount = models.IntegerField(
+        verbose_name='Количество',
+        validators=[MinValueValidator(1)],
+    )
--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    ADMIN = 'admin'
+    USER = 'user'
+    ROLES = [
+        (ADMIN, 'Administrator'),
+        (USER, 'User'),
+    ]
+
+    email = models.EmailField(
+        verbose_name='Адрес электронной почты',
+        max_length=254,
+        unique=True
+    )
+
+    username = models.CharField(
+        verbose_name='Имя пользователя',
+        max_length=150,
+        validators=[AbstractUser.username_validator],
+        unique=True
+    )
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=150,
+    )
+    last_name = models.CharField(
+        verbose_name='Фамилиия',
+        max_length=150,
+    )
+    role = models.CharField(
+        verbose_name='Роль',
+        max_length=50,
+        choices=ROLES,
+        default=USER
+    )
+
+    @property
+    def is_admin(self):
+        """Возвращает роль пользователя: Администратор
+        """
+        return self.role == self.ADMIN
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        ordering = ('id',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
