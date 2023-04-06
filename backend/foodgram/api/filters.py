388670924aecabff90eb74e89c0c8a79ecdd995a
Artem Karpov <ikarp97@gmail.com>
2023-04-06 15:31:45 +0400

    filters

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,12 +1,17 @@
 from django_filters.rest_framework import FilterSet, filters
 
-from recipes.models import Recipe
+from recipes.models import Recipe, Tag
 
 
 class RecipeFilter(FilterSet):
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
         method='filter_is_in_shopping_cart'
+    )
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all(),
     )
 
     class Meta:
